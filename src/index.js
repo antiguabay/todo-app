3329e8e084913d4eba451d01e620981a1a5e1d75
Michael Scott <01mikescott@gmail.com>
2021-01-04 19:40:31 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./Components/TodoContainer"
+import "../src/app.css"
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
